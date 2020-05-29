fd1a845fa05772f057be43b5fbecefe204e7b6e3
exbinario <miguel.4to.1994@gmail.com>
2020-05-29 16:47:28 -0600

    se agregaron los certificados

--- src/server.js
+++ src/server.js
@@ -2,6 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const listaRutas = require('express-list-endpoints');
 
+const path = require('path');
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+var privateKey  = fs.readFileSync(path.join(__dirname, 'sslcert/privkey.pem'), 'utf8');
+
+var certificate = fs.readFileSync(path.join(__dirname, 'sslcert/cert.pem'), 'utf8');
+
+var credentials = {key: privateKey, cert: certificate};
 
 const app = express();
 
@@ -14,7 +23,13 @@ const routes = require('./routes/index');
 app.use(routes);
 
 
-app.listen('3000', () =>{
-    console.log(listaRutas(app));
-    console.log('Server on port 3000');
-})
+// app.listen('3000', () =>{
+//     console.log(listaRutas(app));
+//     console.log('Server on port 3000');
+// })
+
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(80);
+httpsServer.listen(443);
